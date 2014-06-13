ba8187c2dd996b80c4797a682fb5dd4b58746cd5
rLapolli <roberto.lapolli@gmail.com>
2014-06-13 10:27:56 -0300

    teste local

---
+++ WebApplication4-git/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication4_git.MvcApplication" Language="C#" %>
---
+++ WebApplication4-git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication4_git.Startup))]
+namespace WebApplication4_git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
